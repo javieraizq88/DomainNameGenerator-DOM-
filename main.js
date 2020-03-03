cd1dc221c4a9cfb8c4688d1d79923786890de1ce
Javiera Izquierdo M <javieraizquierdo7@gmail.com>
2020-03-03 12:06:51 -0300

    solo info base para partir

---
+++ main.js
@@ -0,0 +1,8 @@
+var pronoun = ['the','our']; 
+var adj = ['great', 'big' ]; 
+var noun = ['jogger','racoon'];
+
+
+let ul = document.getElementById ("dominio");
+let li = document.createElement ("Li");
+let 
---
+++ webpack.config.js
@@ -0,0 +1,9 @@
+let path = require("path");
+
+module.exports = {
+    entry: "./main.js",
+    output: {
+        path: path.resolve(__dirname, "dist"),
+        filename: "main.bundle.js"
+    }
+}
