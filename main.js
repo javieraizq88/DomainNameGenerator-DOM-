8ed9787fac26e152968a0ecd21f0013061b5675a
Javiera Izquierdo M <javieraizquierdo7@gmail.com>
2020-03-03 12:17:05 -0300

    for listo

--- main.js
+++ main.js
@@ -2,7 +2,25 @@ var pronoun = ['the','our'];
 var adj = ['great', 'big' ]; 
 var noun = ['jogger','racoon'];
 
+for (let i=0; i < pronoun.length; i++){
+    for(let j=0; j < adj.length; j++){
+        for(let k=0; k < noun.length; k++){
+            var final = pronoun[i] + adj [j] + noun [k] +".com"
+        }
+    }
+}
+
+
+thegreatjogger.com 
+thegreatracoon.com 
+ourgreatjogger.com 
+ourgreatracoon.com 
+thebigjogger.com 
+thebigracoon.com 
+ourbigjogger.com 
+ourbigracoon.com
+
 
 let ul = document.getElementById ("dominio");
 let li = document.createElement ("Li");
-let 
+let textLi = document.createTextNode ("Hola");
