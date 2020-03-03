ef756f8732c91106560636f4d0ec984a208f45d6
Javiera Izquierdo M <javieraizquierdo7@gmail.com>
2020-03-03 12:28:51 -0300

    final

--- main.js
+++ main.js
@@ -2,25 +2,15 @@ var pronoun = ['the','our'];
 var adj = ['great', 'big' ]; 
 var noun = ['jogger','racoon'];
 
-for (let i=0; i < pronoun.length; i++){
-    for(let j=0; j < adj.length; j++){
-        for(let k=0; k < noun.length; k++){
-            var final = pronoun[i] + adj [j] + noun [k] +".com"
+let ul = document.getElementById ("dominio"); /*creo la lista*/
+    for (let i=0; i < pronoun.length; i++){ /*creo el for para q me lea la info de la var*/
+        for(let j=0; j < adj.length; j++){
+            for(let k=0; k < noun.length; k++){
+                var final = pronoun[i] + adj [j] + noun [k] +".com"; /* para concatenar los for*/
+                let li = document.createElement ("li"); /*para crear cada elemento de la lista*/
+                li.innerHTML = final; /*para agregar la var final cada elemento de lista*/
+                ul .appendChild (li); /*para escribir dentro de la lista el elemento*/
+            }
         }
     }
-}
 
-
-thegreatjogger.com 
-thegreatracoon.com 
-ourgreatjogger.com 
-ourgreatracoon.com 
-thebigjogger.com 
-thebigracoon.com 
-ourbigjogger.com 
-ourbigracoon.com
-
-
-let ul = document.getElementById ("dominio");
-let li = document.createElement ("Li");
-let textLi = document.createTextNode ("Hola");
